f8845849e7b1a6ec4327c0e340d63edb9f73a4d5
yieg77 <45890256+yieg77@users.noreply.github.com>
2025-07-31 23:54:45 +0900

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("Hello, Streamlit!")
-st.write("This is a simple Streamlit app to test GitHub and Streamlit deployment.")
-
-name = st.text_input("What's your name")
-if name :
-    st.write(f"It's you, {name}! 👋")
